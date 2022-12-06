a57788d97721f54d4f39db4418b3d30bdde2e05a
athulya2000 <athulyav00@gmail.com>
2022-12-06 21:13:20 +0530

    view employee

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { ViewEmployeesComponent } from './view-employees/view-employees.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+const myRoute:Routes=[
+  {
+    path:"",
+    component:AddEmployeeComponent
+  },
+  {
+    path:"view",
+    component:ViewEmployeesComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddEmployeeComponent
+    AddEmployeeComponent,
+    ViewEmployeesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute),
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
